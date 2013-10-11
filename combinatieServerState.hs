ff16a0a822d25c95e598f823888f968657b76bf9
Pim Jager <pim@pimjager.nl>
2013-10-11 13:50:59 +0200

    State proof of concept bewaart de data nu in een data-object, ipv primitive

--- combinatieServerState.hs
+++ combinatieServerState.hs
@@ -22,12 +22,14 @@ import qualified Data.ByteString.UTF8 as BU
 import Control.Concurrent (forkIO)
 
 import Data.IORef (IORef, newIORef, atomicModifyIORef)
-import Control.Monad.Trans (liftIO)
+import Control.Monad.Trans (liftIO, lift)
+
+data CData = CI Int 
 
 main :: IO ()
 main = do
 		forkIO serverHttp --de httpserver draait in een apart thread
-		counter <- newIORef 0
+		counter <- newIORef (CI 0)
 		WS.runServer "0.0.0.0" 8080 $ websockets counter --runserver is een extreem simpele server voor de websockets	
 		
 serverHttp :: IO ()
@@ -44,17 +46,17 @@ httpget a req = return $ do
 --	WEBSOCKETS			
 -- RFC6455 heeft de beste browsersupport, zie ook: http://en.wikipedia.org/wiki/WebSocket#Browser_support
 -- zit om een vage reden niet in SebSockets (ondanks dat doc zegt van wel), Hybi00 werkt iig in IE11 en Chrome 29
-websockets :: IORef Int -> WS.Request -> WS.WebSockets WS.Hybi00 ()
+websockets :: IORef CData -> WS.Request -> WS.WebSockets WS.Hybi00 ()
 websockets counter rq = do
 						WS.acceptRequest rq
 						WS.sendTextData $ T.pack "Hallo client, wat wil je met 3 vermenigvuldigen?"
 						vermenigvuldig counter
 					
-vermenigvuldig :: IORef Int -> WS.WebSockets WS.Hybi00 ()
+vermenigvuldig :: IORef CData -> WS.WebSockets WS.Hybi00 ()
 vermenigvuldig counter = forever $ do
 							msg <- WS.receiveData
 							newC <- liftIO $ incCounter counter
-							WS.sendTextData $ T.concat [resultaat $ readMsg msg, T.pack (" | De counter is: " ++ show newC)]
+							WS.sendTextData $ T.concat [resultaat $ readMsg msg, T.pack (" | De counter is: " ++ (show $ readCData newC))]
 							where 
 								readMsg :: T.Text -> Maybe Int
 								readMsg m = let rs = (reads $ T.unpack m)::[(Int, String)] in
@@ -68,5 +70,8 @@ resultaat Nothing  = "Dat is geen leesbaar getal, grapjas"
 resultaat (Just 0) = "0 keer iets is 0, dat weet toch iedereen mallerd"
 resultaat (Just x) = T.pack $ ("Je resultaat is: " ++ (show $ x*3))
 
-incCounter :: IORef Int -> IO Int
-incCounter c = atomicModifyIORef c (\ct -> (ct+1, ct))
+incCounter :: IORef CData -> IO CData
+incCounter c = atomicModifyIORef c (\ct -> ((CI ((readCData ct) +1)), (CI (readCData ct))))
+				
+readCData :: CData -> Int
+readCData (CI n) = n
